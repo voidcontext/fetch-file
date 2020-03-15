e9283ab8c79667dd3e45579abcbb6bd0eb9f25b4
Gabor Pihaj <gabor.pihaj@gmail.com>
2020-03-15 16:50:41 +0000

    Use head instead of dd to create random test file

--- docker/static-files/pre_build.sh
+++ docker/static-files/pre_build.sh
@@ -4,5 +4,5 @@ DIR="$(dirname $0)"
 
 echo "Destination dir: $DIR"
 
-dd if=/dev/random of=$DIR/100MB.bin bs=1024 count=102400
+head -c 104857600 </dev/urandom > $DIR/100MB.bin
 shasum $DIR/100MB.bin | awk '{print $1}' > $DIR/100MB.bin.shasum
