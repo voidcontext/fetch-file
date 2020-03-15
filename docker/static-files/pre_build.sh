00fd2c500ea4e545b4fc45303d013a76749b3600
Gabor Pihaj <gabor.pihaj@gmail.com>
2020-03-15 16:32:01 +0000

    Print the dest dir in pre_build.sh

--- docker/static-files/pre_build.sh
+++ docker/static-files/pre_build.sh
@@ -2,5 +2,7 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
+echo "Destination dir: $DIR"
+
 dd if=/dev/random of=$DIR/100MB.bin bs=1024 count=102400
 shasum $DIR/100MB.bin | awk '{print $1}' > $DIR/100MB.bin.shasum
