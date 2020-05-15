3b438a9e2de474987d1a6e3eaeaf2c9140b39eef
Gabor Pihaj <gabor.pihaj@gmail.com>
2020-05-15 07:50:51 +0100

    Fix pre_build.sh

--- docker/static-files/pre_build.sh
+++ docker/static-files/pre_build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -x
+
 DIR="$(dirname $0)"
 
 echo "Destination dir: $DIR"
@@ -7,7 +9,7 @@ echo "Destination dir: $DIR"
 for size in 10 100; do
   filename=${size}MB.bin
   head -c $(($size * 1048576)) </dev/urandom > $DIR/$filename
-  gzip -k -f $filename
+  gzip -k -f $DIR/$filename
   shasum -a 256 $DIR/$filename | awk '{print $1}' > $DIR/$filename.sha256
   shasum -a 256 $DIR/$filename.gz | awk '{print $1}' > $DIR/$filename.gz.sha256
 done
